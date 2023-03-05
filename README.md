3cb647d3bb2422307bda84b638e1c4361feaf8a6
kennedyisn <kennedyhayles@gmail.com>
2023-03-05 03:23:54 -0500

    Final edits

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Description
   
-This project was created to give a weather forecast for the upcoming work week while also having a recent searches bar for other cities and overall searchbar to find out the forecast for cities in advance. Personally I travel frequently for work and plan my work schedule during regular office hours while working. This application allows me to plan out my travel schedule accordingly right on my laptop. This is could also be useful in trip planning through multiple cities as well, because you would be able to flip between forecasts for multiple cities. In making this project I learned that using js I can recreated css styling dynamically in response to a user's input. This was helpful as it helped me write less code while creating this project.
+This project was created to give a weather forecast for the upcoming work week while also having a recent searches bar for other cities and overall searchbar to find out the forecast for cities in advance. Personally I travel frequently for work and plan my work schedule during regular office hours while working. This application allows me to plan out my travel schedule accordingly right on my laptop. This is could also be useful in trip planning through multiple cities as well, because you would be able to flip between forecasts for multiple cities. In making this project I learned that using jQuery I can recreated css styling dynamically in response to a user's input. This was helpful as it helped me write less code while creating this project.
 
 ## Table of Contents
 
@@ -21,7 +21,7 @@ Simply type in the city that you would like to see the weather forecast for and 
 https://kennedyisn.github.io/Module-6-5-Day-Weather-Forecast
 The following screenshot shows the web application's appearance:
 
-![Weather Dashboard demo](Assets/06-server-side-apis-homework-demo.png)
+![Weather Dashboard demo](Assets/Mod6SS.png)
 
 ## Credits
 
--- script.js
+++ script.js
@@ -1,23 +1,202 @@
-// This page of js is to convert the searched city from the User into coordinates then pass that through to the API to gather results
-var userFormEl = document.querySelector('#user-form')
+var citynameInputEl = document.querySelector('#cityname');
+var apiKey = "5e2ebb8607ff1ac7e13aeaa989acb581";
+var dailyDisplayContainer = document.getElementById("daily-display");
+var dailyForecastEl = document.getElementById("daily-forecastinfo");
+var pastSearches = [];
 
-// Function to retreive Weather Data from searched city
-function getSearchedCitycoord(event) {
-    event.preventDefault();
-
-    //var here will be plugged into a string to create the API link to call back data
-    var searchedCityVal = document.querySelector('').value
-
-    // boolean to report an error if there is nothing in the search bar
-    if (!searchedCityVal) {
-        console.error('Please enter a valid city to search!');
-        return;
-    }
-    // Geocoding API to turn the city searched into coordinates
-    var geocoordString = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchedCityVal + '&limit=1&appid={5e2ebb8607ff1ac7e13aeaa989acb581}'
-
-    location.assign(geocoordString);
+function priorSearches(cityname) {
+  var pastSearch = cityname;
+  localStorage.setItem('savedSearch', pastSearch);
+  console.log(pastSearch);
+  pastSearches.push(pastSearch);
+  console.log('Past weather searches: ' + pastSearches);
+  localStorage.setItem('pastSearches',(pastSearches));
+  createUniqueButtonsFromArray(pastSearches)
 }
 
-// event listener to get the coordinates for the city submitted by the user
-userFormEl.addEventListener('submit', getSearchedCitycoord);
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(event) {
+  console.log("Clicked")
+  event.preventDefault();
+  
+  var cityname = citynameInputEl.value.trim();
+  console.log("City: ", cityname);
+  if (cityname) {
+     geoFind(cityname)
+     priorSearches(cityname)
+    citynameInputEl.value = '';
+  } else {
+    alert('Please enter a City');
+  }
+})
+
+function geoFind(city) {
+  var geoUrl =`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+  fetch(geoUrl) 
+    .then(response => response.json())
+    .then(data => { 
+      console.log(data);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      console.log(lat, lon)
+
+      get5DayWeatherData(lat, lon)
+      getTodaysWeatherData(lat, lon)
+    })
+}
+
+function get5DayWeatherData(lat, lon) {
+    var forecastUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => { 
+      console.log(data);
+      var weeklyData = data;
+      console.log(weeklyData)
+
+      display5DayForecast(weeklyData)
+    })
+}
+
+function getTodaysWeatherData(lat, lon) {
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => { 
+    console.log(data);
+    var dailyData = data;
+    console.log(dailyData)
+
+    displayTodaysForecast(dailyData)
+  })
+}
+
+function display5DayForecast(weeklyData) {
+  console.log(weeklyData)
+
+  var weeklyHeader1 = weeklyData.list[0].dt_txt;
+  $('#day1-header').text(weeklyHeader1);
+
+  var weeklyTemp1 = weeklyData.list[0].main.temp;
+  $('#daily-temp1').text('Temp: ' + weeklyTemp1 + '° F');
+
+  var weeklySpeed1 = weeklyData.list[0].wind.speed;
+  $('#daily-wind1').text(' Wind: ' + weeklySpeed1 + ' MPH');
+    
+  var weeklyHumidity1 = weeklyData.list[0].main.humidity;
+  $('#daily-humidity1').text('Humidity: ' + weeklyHumidity1 + '%');
+
+  var day1Icon = weeklyData.list[0].weather[0].icon
+  var day1URL = 'http://openweathermap.org/img/w/' + day1Icon + '.png';
+  $('#day1-icon').attr('src', day1URL);
+
+  var weeklyHeader2 = weeklyData.list[8].dt_txt;
+  $('#day2-header').text(weeklyHeader2);
+
+  var weeklyTemp2 = weeklyData.list[8].main.temp;
+  $('#daily-temp2').text('Temp: ' + weeklyTemp2 + '° F');
+
+  var weeklySpeed2 = weeklyData.list[8].wind.speed;
+  $('#daily-wind2').text(' Wind: ' + weeklySpeed2 + ' MPH');
+    
+  var weeklyHumidity2 = weeklyData.list[8].main.humidity;
+  $('#daily-humidity2').text('Humidity: ' + weeklyHumidity2 + '%');
+
+  var day2Icon = weeklyData.list[8].weather[0].icon
+  var day2URL = 'http://openweathermap.org/img/w/' + day2Icon + '.png';
+  $('#day2-icon').attr('src', day2URL);
+
+  var weeklyHeader3 = weeklyData.list[16].dt_txt;
+  $('#day3-header').text(weeklyHeader3);
+
+  var weeklyTemp3 = weeklyData.list[16].main.temp;
+  $('#daily-temp3').text('Temp: ' + weeklyTemp3 + '° F');
+
+  var weeklySpeed3 = weeklyData.list[16].wind.speed;
+  $('#daily-wind3').text(' Wind: ' + weeklySpeed3 + ' MPH');
+    
+  var weeklyHumidity3 = weeklyData.list[16].main.humidity;
+  $('#daily-humidity3').text('Humidity: ' + weeklyHumidity3 + '%');
+
+  var day3Icon = weeklyData.list[16].weather[0].icon
+  var day3URL = 'http://openweathermap.org/img/w/' + day3Icon + '.png';
+  $('#day3-icon').attr('src', day3URL);
+
+  var weeklyHeader4 = weeklyData.list[24].dt_txt;
+  $('#day4-header').text(weeklyHeader4);
+
+  var weeklyTemp4 = weeklyData.list[24].main.temp;
+  $('#daily-temp4').text('Temp: ' + weeklyTemp4 + '° F');
+
+  var weeklySpeed4 = weeklyData.list[24].wind.speed;
+  $('#daily-wind4').text(' Wind: ' + weeklySpeed4 + ' MPH');
+    
+  var weeklyHumidity4 = weeklyData.list[24].main.humidity;
+  $('#daily-humidity4').text('Humidity: ' + weeklyHumidity4 + '%');
+
+  var day4Icon = weeklyData.list[24].weather[0].icon
+  var day4URL = 'http://openweathermap.org/img/w/' + day4Icon + '.png';
+  $('#day4-icon').attr('src', day4URL);
+
+  var weeklyHeader5 = weeklyData.list[32].dt_txt;
+  $('#day5-header').text(weeklyHeader5);
+
+  var weeklyTemp5 = weeklyData.list[32].main.temp;
+  $('#daily-temp5').text('Temp: ' + weeklyTemp5 + '° F');
+
+  var weeklySpeed5 = weeklyData.list[32].wind.speed;
+  $('#daily-wind5').text(' Wind: ' + weeklySpeed5 + ' MPH');
+    
+  var weeklyHumidity5 = weeklyData.list[32].main.humidity;
+  $('#daily-humidity5').text('Humidity: ' + weeklyHumidity5 + '%');
+
+  var day5Icon = weeklyData.list[32].weather[0].icon
+  var day5URL = 'http://openweathermap.org/img/w/' + day5Icon + '.png';
+  $('#day5-icon').attr('src', day5URL);
+}
+
+function displayTodaysForecast(dailyData) {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${month}-${day}-${year}`;
+  console.log(currentDate);
+  $('#daily-header').text('Date: ' + currentDate);
+
+  var dailyTemp = dailyData.main.temp;
+  $('#daily-temp').text('Temp: ' + dailyTemp + '° F');
+
+  var dailySpeed = dailyData.wind.speed;
+  $('#daily-wind').text('Wind: ' + dailySpeed + ' MPH');
+    
+  var dailyHumidity = dailyData.main.humidity;
+  $('#daily-humidity').text('Humidity: ' + dailyHumidity + '%');
+
+  var dailyIcon = dailyData.weather[0].icon
+  var iconURL = 'http://openweathermap.org/img/w/' + dailyIcon + '.png';
+  $('#header-icon').attr('src', iconURL);
+}
+
+function createUniqueButtonsFromArray(pastSearches) {
+  let uniqueChars = [...new Set(pastSearches)];
+  if (uniqueChars.length>=9) {
+    uniqueChars.shift();
+  }
+  console.log(uniqueChars);
+  $("#past-searches").empty()
+  $.each(uniqueChars, function(index, cityname) {
+    var button = $('<button/>', {
+      text: cityname,
+      value: cityname,
+      class: 'past-searches',
+      click: function() {
+        // passed button value to geoFind
+        var searchedcityVal = $(this).text();
+        geoFind(searchedcityVal);
+      }
+    });
+      // append to button class
+      $('#past-searches').append(button);
+  });
+}
